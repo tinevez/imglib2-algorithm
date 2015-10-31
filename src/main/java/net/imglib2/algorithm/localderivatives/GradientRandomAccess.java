78baa05701d47c304a5d0e250e053d929b360fdd
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2015-10-31 15:35:17 +0100

    Gradient: forward differences up to accuracy order 6.
    Still have to increase DRYness.
    
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src/main/java/net/imglib2/algorithm/localderivatives/GradientRandomAccess.java
+++ src/main/java/net/imglib2/algorithm/localderivatives/GradientRandomAccess.java
@@ -18,7 +18,7 @@ import Jama.Matrix;
  * . The {@link RandomAccess#get()} method then returns a
  * <code>n &times; 1</code>Jama {@link Matrix}, where <code>n</code> is the
  * dimension of the source.
- * 
+ *
  * @author Jean-Yves Tinevez - 2015
  *
  */
@@ -43,7 +43,7 @@ public class GradientRandomAccess
 	 * differences differentiation. Borders of 1 pixel are added on all sides of
 	 * all dimensions. It is the responsibility of the caller to ensure that the
 	 * source is accessible over this expanded interval.
-	 * 
+	 *
 	 * @param src
 	 *            the source.
 	 * @param interval
@@ -73,7 +73,7 @@ public class GradientRandomAccess
 	 * are added for accuracy orders 2, 4, 6 and 8 respectively on all sides of
 	 * all dimensions. It is the responsibility of the caller to ensure that the
 	 * source is accessible over this expanded interval.
-	 * 
+	 *
 	 * @param src
 	 *            the source.
 	 * @param interval
@@ -88,27 +88,302 @@ public class GradientRandomAccess
 	 */
 	public static final < T extends RealType< T >> RandomAccess< Matrix > centralDifference( final RandomAccessible< T > src, final Interval interval, final int accuracyOrder )
 	{
+		final FinalInterval expandedInterval = Intervals.expand( interval, accuracyOrder / 2 );
 		switch ( accuracyOrder )
 		{
 		case 2:
-			return new Gradient2ndOrderRandomAccess< T >( src, interval );
+			return new GradientCD2ndOrderRandomAccess< T >( src, expandedInterval );
 		case 4:
-			return new Gradient4thOrderRandomAccess< T >( src, interval );
+			return new GradientCD4thOrderRandomAccess< T >( src, expandedInterval );
 		case 6:
-			return new Gradient6thOrderRandomAccess< T >( src, interval );
+			return new GradientCD6thOrderRandomAccess< T >( src, expandedInterval );
 		case 8:
-			return new Gradient8thOrderRandomAccess< T >( src, interval );
+			return new GradientCD8thOrderRandomAccess< T >( src, expandedInterval );
 		default:
 			throw new IllegalArgumentException( "Accuracy order can only be 2, 4, 6 or 8. Was: " + accuracyOrder + "." );
 		}
 	}
 
-	private static class Gradient2ndOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
+	public static final < T extends RealType< T >> RandomAccess<Matrix> forwardDiffereence( final RandomAccessible< T > src, final Interval interval, final int accuracyOrder )
+	{
+		final int n = src.numDimensions();
+		final long[] minmax = new long[ 2 * n ];
+		for ( int d = 0; d < n; d++ )
+		{
+			minmax[ d ] = interval.min( d );
+			minmax[ n + d ] = interval.max( d ) + accuracyOrder;
+		}
+		final FinalInterval expandedInterval = Intervals.createMinMax( minmax );
+		switch ( accuracyOrder )
+		{
+		case 1:
+			return new GradientFD1stOrderRandomAccess< T >( src, expandedInterval );
+		case 2:
+			return new GradientFD2nOrderRandomAccess< T >( src, expandedInterval );
+		case 3:
+			return new GradientFD3rdOrderRandomAccess< T >( src, expandedInterval );
+		case 4:
+			return new GradientFD4thOrderRandomAccess< T >( src, expandedInterval );
+		case 5:
+			return new GradientFD5thOrderRandomAccess< T >( src, expandedInterval );
+		case 6:
+			return new GradientFD6thOrderRandomAccess< T >( src, expandedInterval );
+		default:
+			throw new IllegalArgumentException( "Accuracy order can only be 1, 2, 3, 4, 5 or 6. Was: " + accuracyOrder + "." );
+		}
+	}
+
+	private static class GradientFD6thOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
 	{
 
-		private Gradient2ndOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
+		private GradientFD6thOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
 		{
-			super( src, interval, 2 );
+			super( src, interval );
+		}
+
+		@Override
+		public Matrix get()
+		{
+			for ( int d = 0; d < numDimensions(); d++ )
+			{
+				randomAccess.move( 6, d );
+				final double a6 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a5 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a4 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a3 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a2 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a1 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a0 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				matrix.set( d, 0, -1. / 6. * a6 + 6. / 5. * a5 - 15. / 4. * a4 + 20. / 3. * a3 - 15. / 2. * a2 + 6. * a1 - 49. / 20. * a0 );
+			}
+			return matrix;
+		}
+
+		@Override
+		public RandomAccess< Matrix > copyRandomAccess()
+		{
+			return new GradientFD6thOrderRandomAccess< T >( src, interval );
+		}
+
+		@Override
+		public RandomAccess< Matrix > copy()
+		{
+			return copyRandomAccess();
+		}
+	}
+
+	private static class GradientFD5thOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
+	{
+
+		private GradientFD5thOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
+		{
+			super( src, interval );
+		}
+
+		@Override
+		public Matrix get()
+		{
+			for ( int d = 0; d < numDimensions(); d++ )
+			{
+				randomAccess.move( 5, d );
+				final double a5 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a4 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a3 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a2 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a1 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a0 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				matrix.set( d, 0, 1. / 5. * a5 - 5. / 4. * a4 + 10. / 3. * a3 - 5. * a2 + 5. * a1 - 137. / 60. * a0 );
+			}
+			return matrix;
+		}
+
+		@Override
+		public RandomAccess< Matrix > copyRandomAccess()
+		{
+			return new GradientFD5thOrderRandomAccess< T >( src, interval );
+		}
+
+		@Override
+		public RandomAccess< Matrix > copy()
+		{
+			return copyRandomAccess();
+		}
+	}
+
+	private static class GradientFD4thOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
+	{
+
+		private GradientFD4thOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
+		{
+			super( src, interval );
+		}
+
+		@Override
+		public Matrix get()
+		{
+			for ( int d = 0; d < numDimensions(); d++ )
+			{
+				randomAccess.move( 4, d );
+				final double a4 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a3 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a2 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a1 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a0 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				matrix.set( d, 0, -1. / 4. * a4 + 4. / 3. * a3 - 3. * a2 + 4. * a1 - 25. / 12. * a0 );
+			}
+			return matrix;
+		}
+
+		@Override
+		public RandomAccess< Matrix > copyRandomAccess()
+		{
+			return new GradientFD4thOrderRandomAccess< T >( src, interval );
+		}
+
+		@Override
+		public RandomAccess< Matrix > copy()
+		{
+			return copyRandomAccess();
+		}
+	}
+
+	private static class GradientFD3rdOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
+	{
+
+		private GradientFD3rdOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
+		{
+			super( src, interval );
+		}
+
+		@Override
+		public Matrix get()
+		{
+			for ( int d = 0; d < numDimensions(); d++ )
+			{
+				randomAccess.move( 3, d );
+				final double a3 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a2 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a1 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a0 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				matrix.set( d, 0, 1. / 3. * a3 - 3. / 2. * a2 + 3. * a1 - 11. / 6. * a0 );
+			}
+			return matrix;
+		}
+
+		@Override
+		public RandomAccess< Matrix > copyRandomAccess()
+		{
+			return new GradientFD3rdOrderRandomAccess< T >( src, interval );
+		}
+
+		@Override
+		public RandomAccess< Matrix > copy()
+		{
+			return copyRandomAccess();
+		}
+	}
+
+	private static class GradientFD2nOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
+	{
+
+		private GradientFD2nOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
+		{
+			super( src, interval );
+		}
+
+		@Override
+		public Matrix get()
+		{
+			for ( int d = 0; d < numDimensions(); d++ )
+			{
+				randomAccess.move( 2, d );
+				final double a2 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a1 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				final double a0 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				matrix.set( d, 0, -1. / 2. * a2 + 2. * a1 - 3. / 2. * a0 );
+			}
+			return matrix;
+		}
+
+		@Override
+		public RandomAccess< Matrix > copyRandomAccess()
+		{
+			return new GradientFD2nOrderRandomAccess< T >( src, interval );
+		}
+
+		@Override
+		public RandomAccess< Matrix > copy()
+		{
+			return copyRandomAccess();
+		}
+	}
+
+	private static class GradientFD1stOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
+	{
+
+		private GradientFD1stOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
+		{
+			super( src, interval );
+		}
+
+		@Override
+		public Matrix get()
+		{
+			for ( int d = 0; d < numDimensions(); d++ )
+			{
+				final double a0 = randomAccess.get().getRealDouble();
+				randomAccess.fwd( d );
+				final double a1 = randomAccess.get().getRealDouble();
+				randomAccess.bck( d );
+				matrix.set( d, 0, a1 - a0 );
+			}
+			return matrix;
+		}
+
+		@Override
+		public RandomAccess< Matrix > copyRandomAccess()
+		{
+			return new GradientFD1stOrderRandomAccess< T >( src, interval );
+		}
+
+		@Override
+		public RandomAccess< Matrix > copy()
+		{
+			return copyRandomAccess();
+		}
+	}
+
+	private static class GradientCD2ndOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
+	{
+
+		private GradientCD2ndOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
+		{
+			super( src, interval );
 		}
 
 		@Override
@@ -129,7 +404,7 @@ public class GradientRandomAccess
 		@Override
 		public RandomAccess< Matrix > copyRandomAccess()
 		{
-			return new Gradient2ndOrderRandomAccess< T >( src, interval );
+			return new GradientCD2ndOrderRandomAccess< T >( src, interval );
 		}
 
 		@Override
@@ -139,12 +414,12 @@ public class GradientRandomAccess
 		}
 	}
 
-	private static class Gradient4thOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
+	private static class GradientCD4thOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
 	{
 
-		private Gradient4thOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
+		private GradientCD4thOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
 		{
-			super( src, interval, 4 );
+			super( src, interval );
 		}
 
 		@Override
@@ -169,7 +444,7 @@ public class GradientRandomAccess
 		@Override
 		public RandomAccess< Matrix > copyRandomAccess()
 		{
-			return new Gradient4thOrderRandomAccess< T >( src, interval );
+			return new GradientCD4thOrderRandomAccess< T >( src, interval );
 		}
 
 		@Override
@@ -179,12 +454,12 @@ public class GradientRandomAccess
 		}
 	}
 
-	private static class Gradient6thOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
+	private static class GradientCD6thOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
 	{
 
-		private Gradient6thOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
+		private GradientCD6thOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
 		{
-			super( src, interval, 6 );
+			super( src, interval );
 		}
 
 		@Override
@@ -213,7 +488,7 @@ public class GradientRandomAccess
 		@Override
 		public RandomAccess< Matrix > copyRandomAccess()
 		{
-			return new Gradient6thOrderRandomAccess< T >( src, interval );
+			return new GradientCD6thOrderRandomAccess< T >( src, interval );
 		}
 
 		@Override
@@ -223,12 +498,12 @@ public class GradientRandomAccess
 		}
 	}
 
-	private static class Gradient8thOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
+	private static class GradientCD8thOrderRandomAccess< T extends RealType< T >> extends AbstractGradientRandomAccess< T >
 	{
 
-		private Gradient8thOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
+		private GradientCD8thOrderRandomAccess( final RandomAccessible< T > src, final Interval interval )
 		{
-			super( src, interval, 8 );
+			super( src, interval );
 		}
 
 		@Override
@@ -261,7 +536,7 @@ public class GradientRandomAccess
 		@Override
 		public RandomAccess< Matrix > copyRandomAccess()
 		{
-			return new Gradient8thOrderRandomAccess< T >( src, interval );
+			return new GradientCD8thOrderRandomAccess< T >( src, interval );
 		}
 
 		@Override
@@ -281,13 +556,11 @@ public class GradientRandomAccess
 
 		protected final Interval interval;
 
-		protected AbstractGradientRandomAccess( final RandomAccessible< T > src, final Interval interval, final int accuracyOrder )
+		protected AbstractGradientRandomAccess( final RandomAccessible< T > src, final Interval interval )
 		{
 			this.src = src;
 			this.interval = interval;
-			if ( !( accuracyOrder == 2 || accuracyOrder == 4 || accuracyOrder == 6 || accuracyOrder == 8 ) ) { throw new IllegalArgumentException( "Accuracy order can only be 2, 4, 6 or 8. Was: " + accuracyOrder + "." ); }
-			final FinalInterval expandedInterval = Intervals.expand( interval, accuracyOrder / 2 );
-			randomAccess = src.randomAccess( expandedInterval );
+			randomAccess = src.randomAccess( interval );
 			matrix = new Matrix( src.numDimensions(), 1 );
 		}
 
